8bcd66571a35d1863d31a353a471409fe8d9d1e3
Dann233 <ds005110@gmail.com>
2026-06-09 22:35:07 -0600

    Preparacion de ambiente de trabajo con Vite + React y configuracion de gh pages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/proyectopersonal_c37683-enciclopedia/',
+  plugins: [react()],
+})
