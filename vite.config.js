94a2b7b2b0cf7e6b3c777d8ad11dcd1172429279
Dann233 <ds005110@gmail.com>
2026-06-09 22:43:07 -0600

    fix: corregir base url en vite config para github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/proyectopersonal_c37683-enciclopedia/',
+  base: '/proyectopersonal-enciclopedia-c37683/',
   plugins: [react()],
 })
